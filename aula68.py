699e9103f8e5a8e309f1076ce5d8b4f003c14e3a
Willian Reina Joviano <103294061+SharkWillian@users.noreply.github.com>
2025-02-19 13:23:02 -0300

    aula68.py

---
+++ aula68.py
@@ -0,0 +1,18 @@
+"""
+Escopo é o local onde aquele cod pode atingir
+
+"""
+
+def escopo():
+  x = 1 #Variável definida dentro do escopo da função / Se vc quiser utilizar esse x FORA da função, ocorrerá erros / Variável protegida pela função
+  print(x) #Todo cod dentro da def, está protegido e imanipulável fora da def
+
+
+x = 1
+def escopo():
+  print(x) #Neste caso, o x utilizará do valor fora da Def, isso pode
+
+
+ x = 1          # Escopo FORA do módulo/def
+def escopo():   # Escopo DENTRO do módulo/def
+  print(x)      # Escopo DENTRO do módulo/def
